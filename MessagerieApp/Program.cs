86e6dc0b8f9a83a09a3f0abcc0fcd926196ea81e
bakr-oukessou <bakr.oukessou@gmail.com>
2025-01-31 00:01:44 +0100

    KAYTCOMPILA !!!!

--- MessagerieApp/Program.cs
+++ MessagerieApp/Program.cs
@@ -1,83 +1,59 @@
 using MessagerieApp.Business;
 using MessagerieApp.Business.Interfaces;
 using MessagerieApp.Data;
-using MessagerieApp.Models;
 using MessagerieApp.Repositories;
-using MessagerieApp.Repository;
 using MessagerieApp.Repository.Interfaces;
 using MessagerieApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Récupération de la chaîne de connexion
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-
-DatabaseConnection dbHelper = new DatabaseConnection(connectionString);
-
-// Test de la connexion
-dbHelper.TestConnection();
-
+// Enregistrement des services pour l'injection de dépendances
 builder.Services.AddRazorPages();
 
-// Register Repository and Service
+// Enregistrement de la connexion comme service
+builder.Services.AddSingleton(connectionString);
 
-builder.Services.AddControllersWithViews();   
+// Enregistrement des repositories avec injection manuelle de la chaîne de connexion
 builder.Services.AddScoped<IUserRepository>(provider =>
-    new UserRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IUserService, UserService>();
+    new UserRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<IDemandeRessourceRepository>(provider =>
-    new DemandeRessourceRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IDemandeRessourceService, DemandeRessourceService>();
+    new DemandeRessourceRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<IRessourceRepository>(provider =>
-    new RessourceRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IRessourceService, RessourceService>();
+    new RessourceRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<IDepartementRepository>(provider =>
-    new DepartementRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IDepartementService, DepartementService>();
+    new DepartementRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<ISupplierRepository>(provider =>
-    new SupplierRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<ISupplierService, SupplierService>();
+    new SupplierRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<IMaintenanceDiagnosisRepository>(provider =>
-    new MaintenanceDiagnosisRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IMaintenanceDiagnosisService, MaintenanceDiagnosisService>();
+    new MaintenanceDiagnosisRepository(provider.GetRequiredService<string>()));
 builder.Services.AddScoped<INotificationRepository>(provider =>
-    new NotificationRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
-builder.Services.AddScoped<INotificationService, NotificationService>();
-builder.Services.AddScoped<IRessourceRepository, RessourceRepository>();
-builder.Services.AddScoped<IRessourceService, RessourceService>();
-builder.Services.AddScoped<IMaintenanceDiagnosisRepository, MaintenanceDiagnosisRepository>();
-builder.Services.AddScoped<IMaintenanceDiagnosisService, MaintenanceDiagnosisService>();
+    new NotificationRepository(provider.GetRequiredService<string>()));
 
-builder.Services.AddScoped<DatabaseConnection>();
-builder.Services.AddScoped<RessourceRepository>();
-// Add services to the container.
-builder.Services.AddControllers();
+// Enregistrement des services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IDemandeRessourceService, DemandeRessourceService>();
+builder.Services.AddScoped<IRessourceService, RessourceService>();
+builder.Services.AddScoped<IDepartementService, DepartementService>();
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<IMaintenanceDiagnosisService, MaintenanceDiagnosisService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-
-
-// Configure the HTTP request pipeline.
+// Configuration du pipeline de requêtes HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
